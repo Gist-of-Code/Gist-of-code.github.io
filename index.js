e636eb4746169ed9aa3c554372ecf65613addc66
Travis <tp523130@gmail.com>
2018-09-01 17:44:07 -0700

    Main Pages setup

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const path = require('path')
+var app = express()
+const PORT = process.env.PORT || 5000
+express()
+app.use(express.static(path.join(__dirname, 'public')))
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/public/views/index.html'));
+});
+app.get('/CodeArticles',function(req,res){
+  res.sendFile(path.join(__dirname+'/public/views/codearticles.html'))
+});
+app.get('/CodeTutorials',function(req,res){
+  res.sendFile(path.join(__dirname+'/public/views/codetutorials.html'))
+});
+app.get('/styling/main.css',function(req,res){
+  res.sendFile(path.join(__dirname+'/public/styling/main.css'))
+});
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
