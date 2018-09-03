f96a6dc68680517b1c3f4a40114ac635fd899394
Travis <tp523130@gmail.com>
2018-09-03 14:34:49 -0700

    Google Analytics 8

--- index.js
+++ index.js
@@ -19,4 +19,7 @@ app.get('/styling/main.css',function(req,res){
 app.get('/javascript/gtag.js',function(req,res){
   res.sendFile(path.join(__dirname+'/scripts/javascript/gtag.js'))
 })
+app.get('/scripts/javascript/autotrack.js',function(req,res){
+  res.sendFile(path.join(__dirname+'/autotrack.js'))
+}
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
